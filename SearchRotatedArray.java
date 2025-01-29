6fc3dd285ba3674b0547a8787b32490be3070c80
lovekumar404 <lovechaudhary1090@gmail.com>
2025-01-29 10:54:53 +0530

    Initial commit

---
+++ SearchRotatedArray.java
@@ -0,0 +1,34 @@
+public class SearchRotatedArray{
+    
+    public static int search(int[] nums, int target) {
+        int start=0, end=nums.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target) return mid;
+            
+            if(nums[start]<=nums[mid]){
+                if(nums[start]<=target && target < nums[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && target < nums[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {4,5,6,7,0,1,2};
+        int target = 0;
+
+        System.out.println(search(nums, target));
+        
+    }
+}
